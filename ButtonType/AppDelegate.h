eff141176b2046f138712ed57acae4ddcc24b0e9
coderMyron <244270327@qq.com>
2019-09-24 22:47:45 +0800

    Initial Commit

---
+++ ButtonType/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonType
+//
+//  Created by Myron on 2019/9/24.
+//  Copyright © 2019 Myron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonType/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ButtonType
+//
+//  Created by Myron on 2019/9/24.
+//  Copyright © 2019 Myron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
